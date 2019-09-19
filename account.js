95ef2886f1dbc341376a2f98d815a4d9c12df072
reenesa <susannins@gmail.com>
2019-09-19 15:48:48 +0300

    skeleton of the account fuctions

---
+++ account.js
@@ -0,0 +1,25 @@
+class BankAccount {
+    constructor() {
+        // your code here
+    }
+
+    getBalance() {
+        // your code here
+    }
+
+    open() {
+        // your code here
+    }
+
+    deposit() {
+        // your code here
+    }
+
+    withdraw() {
+        // your code here
+    }
+
+    close(){
+
+    }
+}
